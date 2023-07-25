58a15890d9e2194dc53a3a66268a86347a89730c
ehcodes <engrid.hernandez@gmail.com>
2023-07-25 12:15:55 -0400

    completed loops section

--- script.js
+++ script.js
@@ -93,49 +93,102 @@ print `true` to the console */
 
 /* D. The farm */
 
-1.  Declare a variable `animal`. Set it to be either "cow" or something else
+// 1.  Declare a variable `animal`. Set it to be either "cow" or something else
 let animal = 'cow';
 
-2.  Write code that will print out "mooooo" if the it is equal to `cow`
-3.  Change your code so that if the variable `animal` is anything other than a cow, it will print "Hey! You're not a cow."
+// 2.  Write code that will print out "mooooo" if the it is equal to `cow`
+// 3.  Change your code so that if the variable `animal` is anything other than a cow, it will print "Hey! You're not a cow."
 if(animal==='cow'){
     console.log('mooooo');
 }else{
     console.log(`Hey! You're not a cow.`)
 }
-
-4.  Commit
+// 4.  Commit
 
 /* E. Driver's Ed */
 
 // 1.  Make a variable that holds a person's age; be semantic
-// 2.  Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+let age = 55;
+
+// 2.  Write code that will print out "Here are the keys!", 
+// if the age is 16 years or older, or, if the age is younger than 16,
+// a message should print "Sorry, you're too young."
+if(age > 16){
+    console.log("Here are the keys!")
+}else{
+    console.log("Sorry, you're too young.")
+}
 
 /* II. Loops: */
 
 /* A. The basics */
 
-// 1.  Write a loop that will print out all the numbers from 0 to 10, inclusive
-// 2.  Write a loop that will print out all the numbers from 10 up to and including 400
-// 3.  Write a loop that will print out every third number starting with 12 and going no higher than 4000
-
+// 1.  Write a loop that will print out all the numbers from 0 to 10,
+// inclusive.
+for(let i =0;i<11;i++){
+    console.log(i);
+}
+// 2.  Write a loop that will print out all the numbers from 10 up to
+// and including 400.
+for(let i=10;i<401;i++){
+    console.log(i);
+}
+// 3.  Write a loop that will print out every third number starting
+// with 12 and going no higher than 4000.
+for(let i=12;i<=4000;i++){
+    if(i%3===0){
+        console.log(i);
+    }
+}
 /* B. Get even */
 
 // 1.  Print out the numbers that are within the range of 1 - 100
-// 2.  Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+for(let i=1;i<101;i++){
+// 2.  Adjust your code to add a message next to even numbers only 
+// that says: "<-- is an even number"
+    if(i%2===0){
+        console.log(`${i} <-- is an even number`);
+    }else{
+        console.log(i);
+    }
+}
 
 /* C. Give me Five */
-// 1.  For the numbers 0 - 100, print out "I found a `number`. High five!" if the number is a multiple of five
-// 1.  Add to the code from above to print out "I found a `number`. Three is a crowd" if the number is a multiple of three
-// 1.  For numbers divisible by _both three and five_, be sure your code prints both messages
-
+// 1.  For the numbers 0 - 100, print out "I found a `number`.
+// High five!" if the number is a multiple of five
+// 1.  Add to the code from above to print out "I found a `number`.
+// Three is a crowd" if the number is a multiple of three
+// 1.  For numbers divisible by _both three and five_, be sure
+// your code prints both messages
+for(let i=1;i<101;i++){
+    if(i%5===0){
+        console.log(`I found a ${i}. High five!`);
+    }
+    if(i%3===0){
+        console.log(`I found a ${i}. Three is a crowd.`);
+    }
+}
 /* D. Savings account */
 
-// 1.  Write code that will save the sum of all the numbers between 1 - 10 to a variable called `bank_account`.
+// 1.  Write code that will save the sum of all the numbers between
+// 1 - 10 to a variable called `bank_account`.
 // > Check your work! Your bank_account should have $55 in it.
+let bankAccount = new Array;
+for(let i=1;i<11;i++){
+    bankAccount.push(i);
+}
+bankAccount = bankAccount.reduce((accum,currentVal) => accum+currentVal,0)
+console.log(bankAccount);
 
-// 2.  You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+// 2.  You got a bonus! Your pay is now doubled each week. Write code
+// that will save the sum of all the numbers between 1 - 100 multiplied by 2.
 // > Check your work! Your bank_account should have $10,100 in it.
+bankAccount = new Array;
+for(let i=1;i<101;i++){
+    bankAccount.push(i*2);
+}
+bankAccount = bankAccount.reduce((accum,currentVal) => accum+currentVal,0)
+console.log(bankAccount);
 
 /* III. Arrays & Control flow */
 
